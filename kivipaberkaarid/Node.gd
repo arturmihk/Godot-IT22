ec076cdb7f5367fe4b9bcfe05c8f5340dcfc397c
arturmihk <115464760+arturmihk@users.noreply.github.com>
2023-02-14 11:13:40 +0200

    Add files via upload

--- kivipaberkaarid/Node.gd
+++ kivipaberkaarid/Node.gd
@@ -1,24 +1,96 @@
-#Artur-Mihk Peterso
-#09.02.2023
-#yl8
 extends Node
- 
-var sscore = 0
-var vscore = 0
+var rng = RandomNumberGenerator.new()
 
-const kivi = 0
-const kaaird = 1
-const paber = 2
+var skoorP=0
+var skoorE=0
+
+
+ 
 func _process(delta):
-	if sscore == 10:
-		$svoitsid.text="You WIN!"
-	elif vscore ==10:
-		$svoitsid.text="You LOSE!"
-func _on_kivi_pressed():
+	$TextureRect/punktid1.text = "Skoor: %s" %skoorP
+	$TextureRect/punktid2.text = "Skoor: %s" %skoorE
+	if skoorE == 3:
+		var kaotus = "Mingi TONT voitis"
+		$TextureRect/Tulemus.text = "Tulemus: %s" %kaotus
+		$TextureRect/paber.disabled = true
+		$TextureRect/KIVI.disabled = true
+		$TextureRect/kaarid.disabled = true
+	elif skoorP == 3:
+		var voit = "Sina voitsid"
+		$TextureRect/Tulemus.text = "Tulemus: %s" %voit
+		$TextureRect/paber.disabled = true
+		$TextureRect/KIVI.disabled = true
+		$TextureRect/kaarid.disabled = true
+		
+		
+		
+ 
+func _on_KIVI_pressed():
+	$TextureRect/VALIK1.text = "kivi"
+	rng.randomize()
+	var was = rng.randi_range(1,4)
+	if was == 1:
+		$TextureRect/VALIK2.text="paber"
+	elif was == 2:
+		$TextureRect/VALIK2.text="kaarid"
+	elif was == 3:
+		$TextureRect/VALIK2.text="kivi"
 	
-func _on_paber_pressed():
+	if was == 3:
+		skoorE+=0
+		skoorP+=0
+	elif was == 2:
+		skoorP+=1
+	elif was == 1:
+		skoorE+=1		
+	
 	
 func _on_kaarid_pressed():
+	$TextureRect/VALIK1.text = "kaarid"
+	rng.randomize()
+	var was = rng.randi_range(1,4)
+	if was == 1:
+		$TextureRect/VALIK2.text="paber"
+	elif was == 2:
+		$TextureRect/VALIK2.text="kaarid"
+	elif was == 3:
+		$TextureRect/VALIK2.text="kivi"
+		
+	if was == 2:
+		skoorE+=0
+		skoorP+=0
+	elif was == 1:
+		skoorP+=1
+	elif was == 3:
+		skoorE+=1
+
+func _on_paber_pressed():
+	$TextureRect/VALIK1.text = "paber"
+	rng.randomize()
+	var was = rng.randi_range(1,4)
+	if was == 1:
+		$TextureRect/VALIK2.text="paber"
+	elif was == 2:
+		$TextureRect/VALIK2.text="kaarid"
+	elif was == 3:
+		$TextureRect/VALIK2.text="kivi"
 	
-func _on_restart_pressed():
- get_tree().reload_current_scene()
+	if was == 1:
+		skoorE+=0
+		skoorP+=0
+	elif was == 3:
+		skoorP+=1
+	elif was == 2:
+		skoorE+=1
+		
+		
+ 
+
+	
+	
+
+ 
+func _on_restard_pressed():
+	get_tree().paused = false
+	
+	get_tree().reload_current_scene()
